25ff034372b7869fd76a52b0deeff7572fe5b7ed
NabilCB <124254233+mohd-nabil0@users.noreply.github.com>
2024-05-27 12:10:16 +0530

    First component ReactNative to ReactNativeWeb

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,17 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import {
+  Alert,
+  SafeAreaView,
   StyleSheet,
   Text,
+  TouchableOpacity,
   View,
-  Pressable,
-  useWindowDimensions,
-  Alert,
 } from 'react-native';
+import AppModal from './AppModal';
 
 export default function App() {
-  const {height} = useWindowDimensions();
   const [number, setNumber] = useState(0);
+  const [showModal, setShowModal] = useState(false);
 
   function handlePress() {
     setNumber(parseInt(Math.random() * 10000, 10) % 100);
@@ -18,19 +19,22 @@ export default function App() {
   }
 
   return (
-    <View style={[styles.container, {height}, StyleSheet.absoluteFill]}>
-      <Text>Random number: {number}</Text>
-      <View style={styles.br} />
-      <Pressable
-        style={({pressed}) => [
-          {
-            opacity: pressed ? 0.7 : 1,
-          },
-          styles.btn,
-        ]}
-        onPress={handlePress}>
-        <Text style={styles.btnText}>Generate a number</Text>
-      </Pressable>
+    <View style={styles.container}>
+      {showModal && <AppModal onClose={() => setShowModal(false)}/>}
+      <SafeAreaView>
+        <View style={styles.br} />
+        <View style={styles.randomNumberContainer}>
+          <Text>Random number: {number}</Text>
+          <TouchableOpacity style={styles.btn} onPress={handlePress}>
+            <Text style={styles.btnText}>Generate a number</Text>
+          </TouchableOpacity>
+        </View>
+        <TouchableOpacity
+          style={styles.btn}
+          onPress={() => setShowModal(val => !val)}>
+          <Text style={styles.btnText}>Show Modal</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
     </View>
   );
 }
@@ -39,15 +43,25 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 16,
   },
   br: {
     height: 12,
   },
+  randomNumberContainer: {
+    alignItems: 'center',
+    borderWidth: 0.7,
+    borderColor: 'black',
+    padding: 10,
+    marginVertical: 20,
+  },
   btn: {
+    width: '70%',
+    borderRadius: 10,
     backgroundColor: '#222',
     padding: 10,
+    marginTop: 10,
+    alignItems: 'center',
   },
   btnText: {
     color: '#fff',
---
+++ src/AppModal.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { Modal, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+const AppModal = (props) => {
+
+  return(
+    <Modal
+      animationType="fade"
+      visible={true}
+      transparent={true}
+      statusBarTranslucent={true}
+      onRequestClose={props.onClose}
+    >
+      <View style={styles.modalContainer}>
+        <View style={styles.popupContainer}>
+          <Text style={styles.text}>This is a React Native Modal</Text>
+          <TouchableOpacity
+            style={styles.btn}
+            onPress={props.onClose}>
+            <Text style={styles.btnText}>Close Modal</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </Modal>
+  )
+}
+
+
+const styles = StyleSheet.create({
+  
+  modalContainer: {
+    flex: 1,
+    backgroundColor: "#00000080",
+    justifyContent: "center"
+  },
+  popupContainer: {
+    backgroundColor: 'white',
+    margin: 20,
+    padding: 20,
+    borderRadius: 4,
+    alignItems: 'center'
+  },
+  text: {
+    fontSize: 22,
+    fontStyle: 'bold',
+    color: 'black',
+    textAlign: 'center'
+  },
+  btn: {
+    width: '70%',
+    borderRadius: 10,
+    backgroundColor: '#222',
+    padding: 10,
+    marginTop: 30,
+    alignItems: 'center',
+  },
+  btnText: {
+    color: '#fff',
+  },
+})
+
+export default AppModal;
